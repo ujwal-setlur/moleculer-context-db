7ab6cbf2a3db3c85bbdb18fadecfc9a6dec8cea9
Ujwal Setlur <ujwal.setlur@gmail.com>
2024-01-17 11:47:51 -0800

    exporting MikroOrmDBType type

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import MoleculerMikroContext from './service.databases/moleculer.mikro.context';
 import DatabaseContextManager from './service.middlewares/database.context';
 import MikroConnector from './service.databases/mikro.connector';
 import DatabaseConnector from './service.databases/database.connector';
+import type {MikroOrmDBType} from './service.databases/database.types'
 
 export {
+  MikroOrmDBType,
   DatabaseContextManager,
   MoleculerMikroContext,
   MikroConnector,
